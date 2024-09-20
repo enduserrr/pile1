7b0efa3067c6490f85a704bc7147267825aa5f5d
enduserrr <ai.salo@outlook.com>
2024-09-20 12:19:38 +0300

    cache cleaning script

--- rand_scripts/ascii.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-printf "$(echo {32..126} | xargs printf '\\x%x ')\n"
---
+++ scripts/clear_vscode_cache.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+USER="asalo"
+
+read -p "Are you sure you want to delete all files in /home/${USER}/.var/app/com.visualstudio.code/cache/? (y/n): " confirm
